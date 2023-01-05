5fac8325313cd85530b6bd45be0321216679d782
Monroe Kennedy <monroek@stanford.edu>
2023-01-04 19:49:54 -0800

    upd rviz and launch file for motion between two points example

--- me326_locobot_example/launch/launch_locobot_gazebo.sh
+++ me326_locobot_example/launch/launch_locobot_gazebo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-gnome-terminal -x roslaunch interbotix_xslocobot_gazebo xslocobot_gazebo.launch robot_model:=locobot_wx250s show_lidar:=true use_trajectory_controllers:=true 
+
+# gnome-terminal -x roslaunch interbotix_xslocobot_gazebo xslocobot_gazebo.launch robot_model:=locobot_wx250s show_lidar:=true use_trajectory_controllers:=true 
+
+gnome-terminal -x roslaunch me326_locobot_example locobot_gazebo_example.launch 
 sleep 10
 rosservice call /gazebo/unpause_physics
 
