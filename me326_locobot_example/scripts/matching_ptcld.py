5428f20397968bb017f32be5906fd130939fdb53
Monroe Kennedy <monroek@stanford.edu>
2023-01-31 23:17:45 -0800

    working on service for pixel to point

--- me326_locobot_example/scripts/matching_ptcld.py
+++ me326_locobot_example/scripts/matching_ptcld.py
@@ -10,18 +10,18 @@ from image_geometry import PinholeCameraModel
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import PointStamped
 from visualization_msgs.msg import Marker
-
 import threading
+
+from me326_locobot_example.srv import PixtoPoint, PixtoPointResponse
+
 
 class PixelCloudMatcher:
     def __init__(self, color_image_topic = "/locobot/camera/color/image_raw", depth_image_topic ="/locobot/camera/aligned_depth_to_color/image_raw", depth_img_camera_info="/locobot/camera/aligned_depth_to_color/camera_info"):
         # subscribe to depth image and camera info topics
 
-        self.depth_sub = rospy.Subscriber(depth_image_topic, Image, self.depth_callback)
-        self.info_sub = rospy.Subscriber(depth_img_camera_info, CameraInfo, self.info_callback)
-
-        self.image_sub = rospy.Subscriber(color_image_topic, Image, self.color_image_callback)
         self.bridge = CvBridge()
+        self.depth_image_topic = depth_image_topic
+        self.depth_img_camera_info = depth_img_camera_info
 
         self.image_color_filt_pub = rospy.Publisher("/locobot/camera/block_color_filt_img",Image,queue_size=1)
 
@@ -30,12 +30,15 @@ class PixelCloudMatcher:
 
         self.uv_pix = [0,0] #find the pixel index
 
+        self.color_image_topic = color_image_topic #this is over-written by the service request
 
         self.camera_cube_locator_marker = rospy.Publisher("/locobot/camera_cube_locator",Marker, queue_size=1)
 
         self.point_3d_cloud = PointStamped()
 
+        self.point_3d_cloud_setbool = False #boolean to check if its been properly set
         self.thread_lock = threading.Lock() #threading # self.thread_lock.acquire() # self.thread_lock.release()
+
 
 
     def camera_cube_locator_marker_gen(self):
@@ -48,9 +51,9 @@ class PixelCloudMatcher:
         marker.id = 0
         marker.type = Marker.SPHERE
         # Set the marker scale
-        marker.scale.x = 0.1  # radius of the sphere
-        marker.scale.y = 0.1
-        marker.scale.z = 0.1
+        marker.scale.x = 0.05  # radius of the sphere
+        marker.scale.y = 0.05
+        marker.scale.z = 0.05
 
         # Set the marker pose
         self.thread_lock.acquire()
@@ -78,7 +81,6 @@ class PixelCloudMatcher:
         #Step 2: prep the mask
         mask = cv2.inRange(hsv, lower_bound, upper_bound)
 
-        # print("mask: ", mask)
         index_cube = [[u,v] for u in range(mask.shape[0]) for v in range(mask.shape[1]) if mask[u,v]>0 ]
 
         '''
@@ -90,7 +92,6 @@ class PixelCloudMatcher:
         self.thread_lock.acquire()
         self.uv_pix =[int(center_cube_index[0]),int(center_cube_index[1])]
         self.thread_lock.release()
-        # print("/n /n indx cube", index_cube)
 
         #Step 3: Apply the mask; black region in the mask is 0, so when multiplied with original image removes all non-selected color 
         mask_img = cv2.bitwise_and(color_img, color_img, mask = mask)
@@ -104,38 +105,25 @@ class PixelCloudMatcher:
 
     def depth_callback(self, depth_msg):
         # convert depth image message to a numpy array
-        # print("height,width",depth_msg.height, depth_msg.width)
-        # print("data len:",len(depth_msg.data),"first element:",depth_msg.data[0])
         depth_image = np.frombuffer(depth_msg.data, dtype=np.float32).reshape(depth_msg.height, depth_msg.width)
-        # depth_image = np.frombuffer(depth_msg.data, dtype=np.float64).reshape(depth_msg.height, depth_msg.width)
 
-        # depth_image = np.frombuffer(depth_msg.data).reshape(depth_msg.height, depth_msg.width)
-
-        # print("depth image info: ",type(depth_image),"and size: ",depth_image.shape)
-
-        # loop through all pixels in the image
-        # for y in range(depth_msg.height):
-        #     for x in range(depth_msg.width):
         self.thread_lock.acquire()
         x = self.uv_pix[0]
         y = self.uv_pix[1]
         self.thread_lock.release()
 
         # get the depth value for the current pixel
-
-        # depth = depth_image[y, x] #was original from example
         depth = depth_image[x, y]
 
         # skip pixels with no depth
         if depth == 0:
             pass
+            rospy.loginfo("Skipping cause pixel had no depth")
         else:
             # use the camera model to get the 3D ray for the current pixel
-            # ray = self.camera_model.projectPixelTo3dRay((x, y))
             ray = self.camera_model.projectPixelTo3dRay((y, x))
 
             # calculate the 3D point on the ray using the depth value
-            # print("ray ",ray," depth ",depth, "\n type of ray:",type(ray[0]), " type of depth",type(depth))
             point_3d = np.array(ray) * depth
 
             point_3d_geom_msg = PointStamped()
@@ -144,24 +132,37 @@ class PixelCloudMatcher:
             point_3d_geom_msg.point.y = point_3d[1]
             point_3d_geom_msg.point.z = point_3d[2]
 
-            # print("\npoint_3d",point_3d_geom_msg)
             # transform the point to the pointcloud frame using tf
             point_cloud_frame = self.camera_model.tfFrame()
-            # print("point cloud frame: ", point_cloud_frame)
             self.thread_lock.acquire()
             self.point_3d_cloud = self.listener.transformPoint(point_cloud_frame, point_3d_geom_msg)
+            self.point_3d_cloud_setbool = True
             self.thread_lock.release()
+
             # do something with the point in the pointcloud frame
-            rospy.loginfo("Matched pixel (%d, %d) to point in pointcloud frame: %s", x, y, self.point_3d_cloud)
+            # rospy.loginfo("Matched pixel (%d, %d) to point in pointcloud frame: %s", x, y, self.point_3d_cloud)
 
     def info_callback(self, info_msg):
         # create a camera model from the camera info
         self.camera_model = PinholeCameraModel()
         self.camera_model.fromCameraInfo(info_msg)
 
+    def service_callback(self,req):
+        #this function takes in a requested topic for the image, and returns pixel point
+        self.color_image_topic = req.rgb_img_topic
+        #now call the other subscribers
+        self.depth_sub = rospy.Subscriber(self.depth_image_topic, Image, self.depth_callback)
+        self.info_sub = rospy.Subscriber(self.depth_img_camera_info, CameraInfo, self.info_callback)
+        self.image_sub = rospy.Subscriber(self.color_image_topic, Image, self.color_image_callback)
+        resp = PixtoPointResponse()
+        if self.point_3d_cloud_setbool: 
+            resp.ptCld_point = self.point_3d_cloud
+            return resp
+
 if __name__ == "__main__":
-    rospy.init_node("pixel_cloud_matcher")
+    rospy.init_node("pixel_cloud_matcher_service")
     matcher = PixelCloudMatcher()
+    s = rospy.Service('pix_to_point',PixtoPoint,matcher.service_callback)
     rospy.spin()
 
 '''
